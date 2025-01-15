1169eb87f2b856a423617e18e514d4e1f4a4b10a
BatorGG <bator.karpathegyi@gmail.com>
2025-01-15 14:51:06 +0100

    Update download.js

--- download.js
+++ download.js
@@ -22,23 +22,8 @@ async function downloadYoutubeSection({
       const tempAudio = path.join(tempDir, `temp_audio_${fileName}.m4a`);
 
       checkPermissions(ffmpegPath, 'FFmpeg binary');
-      testFFmpeg();
 
       console.log('Downloading video stream...');
-      console.log('Executing yt-dlp command:', ytDlpPath, [
-        videoUrl,
-        '--add-header', 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        '--no-mtime',
-        '--no-playlist',
-        '--ffmpeg-location', ffmpegPath,
-        '-f', 'bv*[height<=720][fps<=30]',
-        '--download-sections', `*${startTime}-${endTime}`,
-        '--force-keyframes-at-cuts',
-        '--external-downloader', 'ffmpeg',
-        '--external-downloader-args', `ffmpeg_i:-ss ${startTime} -t ${calculateDuration(startTime, endTime)}`,
-        '--cookies', cookiesPath,
-        '-o', tempVideo,
-    ]);
       const downloadVideo = spawn(ytDlpPath, [
           videoUrl,
           '--add-header', 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
@@ -177,11 +162,14 @@ async function downloadHighlight(fileName, id, start, dur) {
             const startTime = formatTime(Math.floor(start))
             const endTime = formatTime(Math.ceil((start + dur)))
             console.log('Starting download process...');
-
+            
+            const downloadpath = path.join(process.cwd(), 'downloads');
+            const outPutFile = fileName + ".mp4"
+            const outputPath = path.join(downloadpath, outPutFile)
             await downloadYoutubeSection({
                 videoUrl: `https://www.youtube.com/watch?v=${id}`,
                 fileName: fileName,
-                outputPath: `./downloads/${fileName}.mp4`,
+                outputPath: outputPath,
                 startTime: startTime,
                 endTime: endTime
             });
